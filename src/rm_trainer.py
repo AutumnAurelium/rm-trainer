ded38d6fd819ac4c3652fdb272bd5a809f68b4d6
AutumnAurelium <88015631+AutumnAurelium@users.noreply.github.com>
2025-02-17 00:43:16 -0500

    re-add margin

--- src/rm_trainer.py
+++ src/rm_trainer.py
@@ -57,8 +57,8 @@ def calculate_loss(model, batch, return_metrics=False):
 
     difference = rewards_chosen - rewards_rejected
     batch_loss = torch.nn.functional.binary_cross_entropy_with_logits(
-        difference,
-        torch.ones_like(difference)
+        difference - batch["margin"],
+        torch.zeros_like(difference)
     ).mean()
         
     if return_metrics:
